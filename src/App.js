8faac63152b82c026d2589ab9fa16b466d0bcabf
Alison Law <alisparklaw@gmail.com>
2020-02-03 13:34:03 +0000

    basic nav and file setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./App.css";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import InmateList from "./components/InmateList";
+import FacilityList from "./components/FacilityList";
+import { Route, Switch } from "react-router-dom";
+import styled from "styled-components";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => (
+  <div>
+    <header>
+      
+      <Navigation />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/InmateList">
+          <InmateList />
+        </Route>
+        <Route exact path="/FacilityList">
+          <FacilityList />
+        </Route>
+      </Switch>
+    </header>
+     </div>
+);
 
 export default App;
