e5ad27e5399a0fcdc529bf7ea41ac9f9ed721e6f
Alison Law <alisparklaw@gmail.com>
2020-02-03 17:35:53 +0000

    recieving token, authorisation build wip

---
+++ src/components/PostAuthDashboard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { navbar } from "materialize-css";
+import styled from "styled-components";
+
+const PostAuthDashboard = () => {
+  return (
+    <div className="navBar">
+      <div>
+        <Link to="/">Home </Link>
+      </div>
+      <div>
+        <Link to="/inmateList">Inmates</Link>
+      </div>
+      <div>
+        <Link to="/facilityList">Facilities </Link>
+      </div>
+      <div>
+        <Link to="/register">Register your facility </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostAuthDashboard;
