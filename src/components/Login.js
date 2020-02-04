a6d926c128031323bb9c0f39a7e2604149efb147
hyetigran <tigran.asriyan@gmail.com>
2020-02-04 19:09:50 +0400

    login page validation and styling with antd

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,77 +1,152 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import styled from "styled-components";
-// import * as Yup from "yup";
-// import { Formik, Form, Field, ErrorMessage } from "formik";
+import "../App.css";
 import { Link } from "react-router-dom";
-import { Form, Icon, Input, Button, Checkbox } from "antd";
+import { Form, Icon, Input, Button, Checkbox, Alert } from "antd";
 
-function LogInForm(props) {
-  console.log(props);
-  const { getFieldDecorator } = props.form;
-  function handleSubmit(values, e) {
+import styled from "styled-components";
+
+const initialLoginValues = {
+  username: "",
+  password: ""
+};
+
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+
+function LoginForm(props) {
+  const [loginValues, setLoginValues] = useState(initialLoginValues);
+  const [loadingUser, setLoadingUser] = useState(false);
+  const [loginError, setLoginError] = useState(false);
+  console.log(loginValues);
+  //antd form dependency
+  const {
+    getFieldDecoratorconst,
+    getFieldDecorator,
+    getFieldsError,
+    isFieldTouched,
+    getFieldError,
+    validateFields
+  } = props.form;
+
+  useEffect(() => {
+    validateFields();
+  }, []);
+  const usernameError = isFieldTouched("username") && getFieldError("username");
+  const passwordError = isFieldTouched("password") && getFieldError("password");
+  const handleChange = e => {
+    setLoginValues({
+      ...loginValues,
+      [e.target.name]: e.target.value
+    });
+  };
+  function handleSubmit(e) {
     e.preventDefault();
+    validateFields((err, loginValues) => {
+      if (!err) {
+        console.log("Received values of form: ", loginValues);
+        doLogIn(loginValues);
+        props.history.push("/");
+      }
+    });
+  }
+
+  const doLogIn = () => {
+    setLoadingUser(true);
     axios
-      .post("https://prisonerbw.herokuapp.com/api/auth/login", values)
+      .post("https://prisonerbw.herokuapp.com/api/auth/login", loginValues)
       .then(response => {
         console.log(response);
-
+        setLoadingUser(false);
         localStorage.setItem("token", response.data.token);
       })
       .catch(e => console.log(e))
       .finally(() => {
         console.log("Axios request finished.");
       });
-  }
+  };
   return (
-    <Form onSubmit={this.handleSubmit} className="login-form">
-      <Form.Item>
-        {getFieldDecorator("username", {
-          rules: [{ required: true, message: "Please input your username!" }]
-        })(
-          <Input
-            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-            placeholder="Username"
+    <StyledContainer>
+      <StyledForm onSubmit={e => handleSubmit(e)} className="login-form">
+        {loginError && typeof loginError === "string" && (
+          <Alert
+            style={{ marginBottom: "1rem" }}
+            message="Failed Login"
+            description={loginError}
+            type="error"
           />
         )}
-      </Form.Item>
-      <Form.Item>
-        {getFieldDecorator("password", {
-          rules: [{ required: true, message: "Please input your Password!" }]
-        })(
-          <Input
-            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-            type="password"
-            placeholder="Password"
-          />
-        )}
-      </Form.Item>
-      <Form.Item>
-        {getFieldDecorator("remember", {
-          valuePropName: "checked",
-          initialValue: true
-        })(<Checkbox>Remember me</Checkbox>)}
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          {getFieldDecorator("username", {
+            rules: [{ required: true, message: "Please input your username" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+              onChange={e => handleChange(e)}
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("password", {
+            rules: [
+              {
+                required: true,
+                min: 6,
+                message: "Password must be 6 characters or more"
+              }
+            ]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+              onChange={e => handleChange(e)}
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("remember", {
+            valuePropName: "checked",
+            initialValue: true
+          })(<Checkbox>Remember me</Checkbox>)}
+          <Button
+            disabled={hasErrors(getFieldsError())}
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            onClick={props.startLoading}
+          >
+            {loadingUser ? "Logging in" : "Log in"}
+          </Button>
+          Or <Link to="/register">Register now!</Link>
+        </Form.Item>
+      </StyledForm>
+    </StyledContainer>
   );
 }
 
-export const WrappedNormalLoginForm = Form.create({ name: "normal_login" })(
-  LogInForm
-);
+export const Login = Form.create({ name: "normal_login" })(LoginForm);
 
-const initialState = {
-  username: "",
-  password: ""
-};
-
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+`;
+const StyledForm = styled(Form)`
+  max-width: 25rem;
+  padding: 2.5rem !important;
+  margin: 2.5rem !important;
+  background: #fbfbfb;
+  border: 1px solid #d9d9d9;
+  border-radius: 6px;
+  @media only screen and (max-width: 600px) {
+    padding: 2.5rem 1.5rem !important;
+    margin: 1.5rem !important;
+  }
+`;
 // const validationSchema = Yup.object().shape({
 //   username: Yup.string().required("Please enter a username"),
 //   password: Yup.string().required("Please enter a password")
