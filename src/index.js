60c44bc605a65b796386fece970a8413d2153a38
hyetigran <tigran.asriyan@gmail.com>
2020-02-04 19:36:06 +0400

    removed commented code

--- src/index.js
+++ src/index.js
@@ -3,23 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-<<<<<<< HEAD
 import { BrowserRouter as Router } from "react-router-dom";
 
-
 ReactDOM.render(
-<Router>
-<App />
-</Router>,
-
-document.getElementById("root"));
-=======
-import{ BrowserRouter as Router} from "react-router-dom";
-
-ReactDOM.render(
-
-   <Router>
+  <Router>
     <App />
-    </Router>, document.getElementById("root"));
->>>>>>> 786706fd578135535c2219aa79181102078a8928
+  </Router>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
