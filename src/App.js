5a58a5f8fa7ea5ff85863bed14779a4a35f23be1
Kolade-Junaid <koladejun@yahoo.com>
2020-02-04 07:07:50 +0000

    Mdified the LogInForm.js and HomePage.js files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import LogInForm from "./components/LogInForm";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LogInForm />
+      <nav>
+        <Link to="/">
+          <h1 className="log-in">Log In</h1>
+        </Link>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/LogIn"></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
